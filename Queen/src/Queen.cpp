e47cc98cd59b64d1ebfa37b9957a216e9fa31c2b
sky2464 <sky2464@Navid-PC>
2018-02-19 16:28:46 -0500

    First Commit

---
+++ Queen/src/Queen.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Queen.cpp
+// Author      : Navid Saboori
+// Version     :
+// Copyright   : Sky2464
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "8 Queens in the board" << endl; // prints 8 Queens in the board
+	return 0;
+}
